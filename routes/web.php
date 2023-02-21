c1e9942a17fc8a40b0c1550c010cfb5d3e47802b
faissal20 <faissal@kreatinc.com>
2023-02-21 11:11:57 +0100

    first commit

---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/redirect', function (Request $request) {  
+    
+    // get all headers from the request
+    $url = $request->url;
+    
+
+    $query = http_build_query([
+        'client_id' => '753862712981672',
+        'redirect_uri' => 'http://localhost:8000/callback',
+        'response_type' => 'code',
+        'scope' => '',
+    ]);
+    session(['subdomain' => $url]);
+    return redirect('https://www.facebook.com/v16.0/dialog/oauth?'.$query);
+});
+
+
+Route::get('/callback', function(Request $request){
+    return redirect('http://' . session('subdomain') . '/callback?' . $request->getQueryString());
+});
+
+Route::get('/', function () {
+    return view('welcome');
+});
